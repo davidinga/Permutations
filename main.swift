17c6752777f3ee624a8f962792e43a87b0cb5212
David Inga <davidinga@gmail.com>
2023-02-28 06:24:54 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,46 @@
+/*
+Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+
+Example 2:
+
+Input: nums = [0,1]
+Output: [[0,1],[1,0]]
+
+Example 3:
+
+Input: nums = [1]
+Output: [[1]]
+
+Constraints:
+
+1 <= nums.length <= 6
+-10 <= nums[i] <= 10
+All the integers of nums are unique.
+*/
+
+func permute(_ nums: [Int]) -> [[Int]] {
+    var result: [[Int]] = []
+    var slate = nums
+
+    func helper(_ i: Int, _ slate: inout [Int]) {
+        guard i < nums.count else {
+            result.append(slate)
+            return 
+        }
+
+        for j in i..<nums.count {
+            slate.swapAt(i, j)
+            helper(i + 1, &slate)
+            slate.swapAt(i, j)
+        }
+    }
+
+    helper(0, &slate)
+
+    return result
+}
